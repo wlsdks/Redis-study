40906dc067f08067f3c9c369ed4c568874da0018
jinan <wlsdks12@naver.com>
2023-06-21 08:21:32 +0900

    1. 레디스 캐시설정 완료

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -2,7 +2,9 @@ package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching // 캐시를 사용하겠다고 알린다.
 @SpringBootApplication
 public class HelloworldApplication {
 
--- src/main/java/com/example/helloworld/service/ExternalApiService.java
+++ src/main/java/com/example/helloworld/service/ExternalApiService.java
@@ -1,5 +1,6 @@
 package com.example.helloworld.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +26,8 @@ public class ExternalApiService {
         return "";
     }
 
+    // cacheNames는 임의로 지정하면 된다. key값도 어떤것을 사용할지 파라미터에서 지정할 수 있다.
+    @Cacheable(cacheNames = "userAgeCache", key = "#userId")
     public int getUserAge(String userId) {
         //외부 서비스나 DB 호출
         try {

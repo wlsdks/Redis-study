a20787db18a67040325bf482c39ff87388d284fe
jinan <wlsdks12@naver.com>
2023-06-20 23:31:26 +0900

    1.추가코드

--- src/main/java/com/example/helloworld/controller/LoginController.java
+++ src/main/java/com/example/helloworld/controller/LoginController.java
@@ -1,2 +1,28 @@
-package com.example.helloworld.controller;public class LoginController {
+package com.example.helloworld.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class LoginController {
+
+    HashMap<String, String> sessionMap = new HashMap<>();
+
+    @GetMapping("/login")
+    public String login(HttpSession session, @RequestParam String name) {
+        sessionMap.put(session.getId(), name);
+
+        return "saved.";
+    }
+
+    @GetMapping("/myName")
+    public String myName(HttpSession session) {
+        String myName = sessionMap.get(session.getId());
+
+        return myName;
+    }
 }

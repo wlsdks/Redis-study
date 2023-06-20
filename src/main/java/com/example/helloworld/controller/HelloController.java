7ed675efa153c013728fd4b4d26b184c251c8da9
jinan <wlsdks12@naver.com>
2023-06-20 22:45:58 +0900

    1.first commit

---
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.controller;
+
+//Hello => "hello world!"
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+}

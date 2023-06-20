9d17a61dbefebcfaeec93f848d2fb1a62273d645
jinan <wlsdks12@naver.com>
2023-06-20 22:53:51 +0900

    1. 코드 변경사항 추가

--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -2,14 +2,34 @@ package com.example.helloworld.controller;
 
 //Hello => "hello world!"
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final StringRedisTemplate redisTemplate;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
     }
+
+    // setFruit?name=banana
+
+    // getFruit
+
+    public String setFruit(@RequestParam String name) {
+
+    }
+
+
+
+
+
 }
